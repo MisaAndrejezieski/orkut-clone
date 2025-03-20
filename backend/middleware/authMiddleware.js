2aa1d17096fccf9e5af793b8c095caddcc01c3a5
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2025-03-20 15:55:19 -0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,19 +1,25 @@
-// backend/middleware/authMiddleware.js
-const { admin } = require('../firebaseAdmin');
+const { admin } = require('../config/firebaseAdmin');
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: 'Acesso negado. Token não fornecido.' });
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso negado. Token não fornecido.' });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const userDoc = await admin.firestore().collection('users').doc(decodedToken.uid).get();
+
+    if (!userDoc.exists) {
+      return res.status(403).json({ message: 'Usuário não encontrado.' });
     }
 
-    try {
-        const decodedToken = await admin.auth().verifyIdToken(token);
-        req.user = decodedToken;
-        next();
-    } catch (error) {
-        res.status(400).json({ message: 'Token inválido.' });
-    }
+    const userData = userDoc.data();
+    req.user = { ...decodedToken, role: userData.role }; // Adiciona a role ao req.user
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Token inválido.' });
+  }
 };
 
 module.exports = authMiddleware;

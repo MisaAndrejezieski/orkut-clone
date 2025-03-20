942176c3a9f2f1d5d72c4fc2786928a14ab8dc4f
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2025-03-20 15:18:39 -0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,30 +1,19 @@
-// Importa o jsonwebtoken para verificar tokens JWT
-const jwt = require('jsonwebtoken');
+// backend/middleware/authMiddleware.js
+const { admin } = require('../firebaseAdmin');
 
-// Middleware de autenticação
-const authMiddleware = (req, res, next) => {
-    // Obtém o token do cabeçalho da requisição e remove a palavra "Bearer"
+const authMiddleware = async (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-
-    // Verifica se o token foi fornecido
     if (!token) {
         return res.status(401).json({ message: 'Acesso negado. Token não fornecido.' });
     }
 
     try {
-        // Verifica se o token é válido e decodifica o payload
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Adiciona o payload decodificado ao objeto de requisição (req)
-        req.user = decoded;
-
-        // Passa para o próximo middleware ou rota
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken;
         next();
     } catch (error) {
-        // Retorna um erro caso o token seja inválido
         res.status(400).json({ message: 'Token inválido.' });
     }
 };
 
-// Exporta o middleware para ser usado nas rotas
 module.exports = authMiddleware;

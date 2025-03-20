e4995eb3f1ab010913a026995ddcef1881e3e0b3
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2025-03-20 15:42:10 -0300

    Create script.js

---
+++ fontend/script.js
@@ -0,0 +1,20 @@
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      alert('Login bem-sucedido!');
+      console.log(data);
+    } catch (error) {
+      alert('Erro ao fazer login.');
+      console.error(error);
+    }
+  });
+  
